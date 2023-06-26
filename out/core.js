6cb01ac250df1ba99d0aa3e4993653b1a133364f
zwf <871772825@qq.com>
2023-06-26 16:38:12 +0800

    fix：优先选中内容

--- out/core.js
+++ out/core.js
@@ -3,11 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.insertConsoleLog = void 0;
 const vscode = require("vscode");
 let supportedLangs = [
-    'javascript',
-    'typescript',
-    'javascriptreact',
-    'typescriptreact',
-    'vue'
+    "javascript",
+    "typescript",
+    "javascriptreact",
+    "typescriptreact",
+    "vue",
 ];
 const insertConsoleLog = () => {
     // 获取当前编辑器的选区
@@ -21,19 +21,21 @@ const insertConsoleLog = () => {
         //   const column = selection.active.character;
         // 获取当前行的缩进，用于在插入console.log语句时保留原有缩进
         const indentMatch = editor.document.lineAt(line).text.match(/^\s*/);
-        const indent = indentMatch ? indentMatch[0] : '';
+        const indent = indentMatch ? indentMatch[0] : "";
         // 获取当前行的文本，用于判断当前行是否有变量定义
         const lineText = editor.document.lineAt(line).text;
         // 通过正则表达式匹配当前行的变量定义
         const variable = lineText.match(/\b(let|var|const)\s+([a-zA-Z_$][0-9a-zA-Z_$]*)\b/);
         // 根据变量定义和缩进的信息，插入console.log语句
         editor.edit((editBuilder) => {
-            if (variable) {
-                editBuilder.insert(new vscode.Position(line + 1, 0), `${indent}console.log('${variable[2]}:', ${variable[2]})\n`);
+            // 优先选中文本
+            if (text) {
+                editBuilder.insert(new vscode.Position(line + 1, 0), `${indent}console.log('${text}:', ${text})\n`);
             }
             else {
-                ;
-                text && editBuilder.insert(new vscode.Position(line + 1, 0), `${indent}console.log('${text}:', ${text})\n`);
+                if (variable) {
+                    editBuilder.insert(new vscode.Position(line + 1, 0), `${indent}console.log('${variable[2]}:', ${variable[2]})\n`);
+                }
             }
         });
     }
